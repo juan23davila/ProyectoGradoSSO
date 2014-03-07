dfc1a8901e077862c6a5e0fb62d21935b116a19f
Andres Herrera <anfho93@gmail.com>
2014-03-07 11:23:52 -0500

    Cambio en URL de WebTest

--- SP1/protected/tests/WebTestCase.php
+++ SP1/protected/tests/WebTestCase.php
@@ -4,13 +4,11 @@
  * Change the following URL based on your server configuration
  * Make sure the URL ends with a slash so that we can use relative URLs in test cases
  */
-define('TEST_BASE_URL','http://localhost/testdrive/index-test.php/');
-
+define('TEST_BASE_URL','http://sp1.anfho.com/testdrive/index-test.php/');
 /**
  * The base class for functional test cases.
  * In this class, we set the base URL for the test application.
- * We also provide some common methods to be used by concrete test classes.
- */
+ * We also provide some common methods to be used by concrete test classes. */
 class WebTestCase extends CWebTestCase
 {
 	/**
