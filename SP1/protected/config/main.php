0081b2fd6a97ead1fe58c73280c39345a5b77a31
Juan Davila <juan23davila@MacBook-Pro-de-Juan.local>
2014-03-10 10:03:39 -0500

    commit
    a

--- SP1/protected/config/main.php
+++ SP1/protected/config/main.php
@@ -5,7 +5,6 @@
 
 // This is the main Web application configuration. Any writable
 // CWebApplication properties can be configured here.
-//Cambio marica, perdon de andres
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
 	'name'=>'My Web Application',
@@ -23,7 +22,7 @@ return array(
 		// uncomment the following to enable the Gii tool
 		/*
 		'gii'=>array(
-			'class'=>'system.gii.GiiModule',
+			'class'=>'system.gii.GiiModule', 
 			'password'=>'Enter Your Password Here',
 			// If removed, Gii defaults to localhost only. Edit carefully to taste.
 			'ipFilters'=>array('127.0.0.1','::1'),
